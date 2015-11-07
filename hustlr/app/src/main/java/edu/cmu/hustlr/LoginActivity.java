3b1aabc490416a577108cbf97cf89e867f8926ce
Jacob Nelson <jacobnelson@Jacobs-MacBook-Pro.local>
2015-11-06 21:15:12 -0500

    Fixed a tiny error

--- hustlr/app/src/main/java/edu/cmu/hustlr/LoginActivity.java
+++ hustlr/app/src/main/java/edu/cmu/hustlr/LoginActivity.java
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         );
 
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -106,13 +106,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
