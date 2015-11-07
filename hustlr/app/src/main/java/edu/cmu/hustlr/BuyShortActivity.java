8caa703022d4ed7b6e6f6e9b2f08245254fe1400
rueiminl <rueiminl@andrew.cmu.edu>
2015-11-06 19:31:01 -0500

    connect buy page to others

--- hustlr/app/src/main/java/edu/cmu/hustlr/BuyShortActivity.java
+++ hustlr/app/src/main/java/edu/cmu/hustlr/BuyShortActivity.java
@@ -1,7 +1,10 @@
 package edu.cmu.hustlr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BuyShortActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class BuyShortActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_short);
+
+        Button buttonBuy = (Button)findViewById(R.id.buttonBuy);
+        buttonBuy.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(BuyShortActivity.this, AccountHomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        Button buttonCancel = (Button)findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(BuyShortActivity.this, AccountHomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
