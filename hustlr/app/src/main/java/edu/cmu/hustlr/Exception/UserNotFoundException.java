cfac78dfe87b6d474459c9c6e9ad7b59e0063656
jcarlnel <jcnelson@andrew.cmu.edu>
2015-11-24 22:55:29 -0500

    Updated and added Exception classes

--- hustlr/app/src/main/java/edu/cmu/hustlr/Exception/UserNotFoundException.java
+++ hustlr/app/src/main/java/edu/cmu/hustlr/Exception/UserNotFoundException.java
@@ -1,7 +1,24 @@
 package edu.cmu.hustlr.Exception;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by jacobnelson on 11/13/15.
  */
-public class UserNotFoundException {
+public class UserNotFoundException extends Throwable{
+
+    private Context myContext;
+
+    public UserNotFoundException(Context context){
+        myContext = context;
+    }
+
+    @Override
+    public void printStackTrace() {
+        Toast.makeText(myContext, "This user could not be found", Toast.LENGTH_LONG).show();
+        super.printStackTrace();
+    }
+
+
 }
