c550297372a214d1ef9bd292bc6ec6c0520235b5
ReachAayush <reach.aayush@gmail.com>
2015-11-26 18:12:42 -0500

    implemented login, working on signup

--- Hustlr_Backend/app.js
+++ Hustlr_Backend/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql      = require('mysql');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//DATABASE LOGIC
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hustlr',
+  password : 'hustle',
+  database : 'hustlr_db'
+});
+
+var createUserTable = 'CREATE TABLE IF NOT EXISTS users (id MEDIUMINT NOT NULL AUTO_INCREMENT, username CHAR(50) NOT NULL, password CHAR(50) NOT NULL, portfolio_id MEDIUMINT, PRIMARY KEY (id));'
+
+var createPortfolioTable = 'CREATE TABLE IF NOT EXISTS portfolio (id MEDIUMINT NOT NULL AUTO_INCREMENT, cash INTEGER NOT NULL, PRIMARY KEY (id));'
+
+var createOwnedStocksTable = 'CREATE TABLE IF NOT EXISTS owned_stock (id MEDIUMINT NOT NULL AUTO_INCREMENT, portfolio_id MEDIUMINT, symbol CHAR(5) NOT NULL, start_price DECIMAL(5,2), is_short TINYINT(1), PRIMARY KEY (id));'
+
+var createTransactionTable = 'CREATE TABLE IF NOT EXISTS transaction (id MEDIUMINT NOT NULL AUTO_INCREMENT, owned_stock_id MEDIUMINT, portfolio_id MEDIUMINT, final_price DECIMAL(5,2), PRIMARY KEY (id));'
+
+connection.connect();
+
+connection.query(createUserTable, function(err, rows, fields) {
+  if (err) throw err;
+  console.log("CREATED USER TABLE!");
+});
+
+connection.query(createPortfolioTable, function(err, rows, fields) {
+  if (err) throw err;
+  console.log("CREATED PORTFOLIO TABLE!");
+});
+
+connection.query(createOwnedStocksTable, function(err, rows, fields) {
+  if (err) throw err;
+  console.log("CREATED OWNED_STOCK TABLE!");
+});
+
+connection.query(createTransactionTable, function(err, rows, fields) {
+  if (err) throw err;
+  console.log("CREATED TRANSACTION TABLE!");
+});
+
+connection.end();
 
 app.use('/', routes);
 app.use('/users', users);
--- Hustlr_Backend/routes/index.js
+++ Hustlr_Backend/routes/index.js
@@ -1,9 +1,87 @@
 var express = require('express');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hustlr',
+  password : 'hustle',
+  database : 'hustlr_db'
+});
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function sendJson(jsonData, res){
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify(jsonData));
+}
+
+function checkIfUserExists(username){
+   var queryString = 'SELECT * from users WHERE username=\'' + username + '\'';
+   connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+
+      if (rows.length == 0) return true;
+      return false;
+   });
+}
+
+function validatePassword(username, password){
+   var queryString = 'SELECT password from users WHERE username=\'' + username + '\'';
+   connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+
+      if (rows[0] == password) return true;
+      return false;
+   });  
+}
+
+function createUserPortfolio(cash){
+   var queryString = 'SELECT password from users WHERE username=\'' + username + '\'';
+   connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+
+      if (rows[0] == password) return true;
+      return false;
+   });  
+}
+
+function createUser(username, password){
+
+}
+
+/* handle get request for login attempt */
+router.get('/login', function(req, res, next) {
+   var username = req.query.username;
+   var password = req.query.password;
+   console.log("User " + username + " tried logging in with password: " + password);
+   //check if user exists
+   if(checkIfUserExists(username)){
+      //validate password
+      if(validatePassword(username, password)){
+         sendJson({result: 'success'}, res);
+         return;
+      }
+      sendJson({result: 'fail', reason: 'user exists but password incorrect'}, res);
+      return;
+   }
+   sendJson({result: 'fail', reason: 'user does not exist'}, res);
+   return;
 });
+
+/* handle get request for signup attempt */
+router.get('/signup', function(req, res, next) {
+   var username = req.query.username;
+   var password = req.query.password;
+   console.log("User " + username + " tried signing up with password: " + password);
+   
+   //check if user exists
+   if(checkIfUserExists(username)){
+      return sendJson({result: 'fail', reason: 'user already exists'}, res);
+   }
+   //if user doesnt exist create user and create user's portfolio
+   sendJson({result: 'success'}, res);
+});
+
+
 
 module.exports = router;
